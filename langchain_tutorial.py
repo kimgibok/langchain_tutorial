e63b94754dec32337b90a2637aff077f16c7a6cd
kimgibok <ruddjs33@naver.com>
2024-03-13 11:42:59 +0900

    langchain_search_crawling

--- langchain_tutorial.py
+++ langchain_tutorial.py
@@ -81,18 +81,19 @@ document_chain = create_stuff_documents_chain(llm, prompt)
 
 from langchain_core.documents import Document
 
-document_chain.invoke({
-    "input": "국민취업지원제도가 뭐야",
-    "context": [Document(page_content="""국민취업지원제도란?
+# document_chain.invoke({
+#     "input": "국민취업지원제도가 뭐야",
+#     "context": [Document(page_content="""국민취업지원제도란?
 
-취업을 원하는 사람에게 취업지원서비스를 일괄적으로 제공하고 저소득 구직자에게는 최소한의 소득도 지원하는 한국형 실업부조입니다. 2024년부터 15~69세 저소득층, 청년 등 취업취약계층에게 맞춤형 취업지원서비스와 소득지원을 함께 제공합니다.
-[출처] 2024년 달라지는 청년 지원 정책을 확인하세요.|작성자 정부24""")]
-})
+# 취업을 원하는 사람에게 취업지원서비스를 일괄적으로 제공하고 저소득 구직자에게는 최소한의 소득도 지원하는 한국형 실업부조입니다. 2024년부터 15~69세 저소득층, 청년 등 취업취약계층에게 맞춤형 취업지원서비스와 소득지원을 함께 제공합니다.
+# [출처] 2024년 달라지는 청년 지원 정책을 확인하세요.|작성자 정부24""")]
+# })
+
 
 from langchain.chains import create_retrieval_chain
 
 retriever = vector.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-response = retrieval_chain.invoke({"input": "상담센터 전화번호 뭐야?"})
+response = retrieval_chain.invoke({"input": "사업추진체계에 대해 알려줘"})
 print(response["answer"])
